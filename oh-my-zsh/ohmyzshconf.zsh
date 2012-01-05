a16d4b0c0b53c493113b850a40f3e95066528406
Piyush Revuri <prevuri14@gmail.com>
2012-01-05 15:02:25 -0500

    Zsh source reorg

---
+++ oh-my-zsh/ohmyzshconf.zsh
@@ -0,0 +1,11 @@
+# OH_MY_ZSH CONFIG
+# Path to your oh-my-zsh configuration.
+export ZSH=$HOME/.oh-my-zsh
+
+# Set name of the theme to load for oh-my-zsh
+ZSH_THEME="miloshadzic"
+
+# plugins for oh-my-zsh
+plugins=(git ruby rails brew pip gem github rvm node osx)
+
+source $ZSH/oh-my-zsh.sh
---
+++ todo.txt/todoconf.zsh
@@ -0,0 +1,2 @@
+export TODOTXT_DEFAULT_ACTION=ls
+export TODOTXT_SORT_COMMAND='env LC_COLLATE=C sort -k 2,2 -k 1,1n'
