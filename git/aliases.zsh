460efaf197681516492ea1418775986118fe13bf
Piyush Revuri <prevuri14@gmail.com>
2012-03-23 17:00:38 -0400

    Changed glog alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,1 +1,1 @@
-alias glog="git log --graph --max-count=10 --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias glog="git log --graph --max-count=20 --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
