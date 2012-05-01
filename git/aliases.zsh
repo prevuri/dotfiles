5455a0b3d6e76024343fa24da94caacb3af4e20f
Piyush Revuri <prevuri14@gmail.com>
2012-05-01 16:29:46 -0400

    Added Git Aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,1 +1,5 @@
 alias glog="git log --graph --max-count=20 --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gl='git pull --prune'
+alias gp='git push origin HEAD'
+alias gd='git diff'
+alias gs='git status -sb'
